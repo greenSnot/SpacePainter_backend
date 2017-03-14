21b21e4c4d3dd1d6fdaf6b466d4850f1d2857760
Patrick <0x00111111@gmail.com>
2017-03-14 13:49:40 +0800

    mongo redis

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 cp config.py.example config.py
 // change config.py
 
-python build_config.py
+// run container
+python ./build_config.py
 docker build -t ubuntu:init .
-docker run -p 29017:29017 -d ubuntu:init
+docker run -t -p 29017:29017 -p 6002:6002 -p 8081:80 --name www -d ubuntu:init
+
+// go into container
+docker exec -it www /bin/bash
+
+// stop container
+docker stop www
+docker rm www
--- build_config.py
+++ build_config.py
@@ -18,7 +18,11 @@ def write(filename, content, append=False):
 createDir(cur_path + '/build')
 
 execfile(cur_path + '/config.py')
+write(cur_path + '/Dockerfile', config['docker_file'])
 write(cur_path + '/build/docker_config', json.dumps(config['docker']))
-write(cur_path + '/build/mongo_config', config['mongo']['command'])
-write(cur_path + '/build/redis_config', json.dumps(config['redis']))
+write(cur_path + '/build/mongo_command', config['mongo']['server']['command'])
+write(cur_path + '/build/http_config.json', json.dumps(config['http']))
+write(cur_path + '/build/wechat_config.json', json.dumps(config['wechat']))
+write(cur_path + '/build/mongo_client.json', json.dumps(config['mongo']['client']))
+write(cur_path + '/build/redis_client.json', json.dumps(config['redis']['client']))
 write(cur_path + '/build/sources.list', config['apt_sources'])
