a83a7f466bb355b6d5080cf66df3dfcc81cb8fd9
Patrick <0x00111111@gmail.com>
2017-04-18 16:59:22 +0800

    user info

--- www/routes/user.js
+++ www/routes/user.js
@@ -5,13 +5,11 @@ var db = require('../db/mongo_schema');
 router.post('/info', function(req, res) {
   db.Users.findOne({
     _id: req.session.user_id,
-  }).exec().then(function(u) {
+  }).select(['_id', 'wechat']).exec().then(function(u) {
     if (u) {
-      var data = u.wechat;
-      data.id = req.session.user_id;
       res.json({
         code: 0,
-        data: data
+        data: u
       });
     } else {
       res.json({
